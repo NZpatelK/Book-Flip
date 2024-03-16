29605b200ed1530274bccffe32340df357354bc7
NZpatelK <patelk.dev@hotmail.com>
2024-03-17 10:31:14 +1300

    initial bookflip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,159 @@
-import Image from "next/image";
+'use client';
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+// import Profile from "./components/Profile";
+// import Contents from "./components/Contents";
+// import AboutMe from "./components/AboutMe";
+// import Skills from "./components/Skills";
+// import Services from "./components/Services";
+// import Projects from "./components/Projects";
+// import ContactUs from "./components/ContactUs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Home() {
+
+  const [windowSize, setWindowSize] = useState({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0
+  });
+
+  const [isBigScreen, setIsBigScreen] = useState(true);
+
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsBigScreen(window.innerWidth >= 1180 && window.innerHeight >= 820);
+    };
+
+    checkScreenSize(); // Check initial screen size
+
+    window.addEventListener('resize', checkScreenSize); // Update on resize
+
+    return () => window.removeEventListener('resize', checkScreenSize); // Clean up event listener
+  }, []);
+
+  // const handleSelectContent = ((pageId: number) => {
+  //   const pages = document.querySelectorAll(`.${styles["book-page"]}.${styles["page-right"]}`);
+  //   pages.forEach((page: any, index: number) => {
+  //     setTimeout(() => {
+  //       if (index <= pageId) {
+  //         page.classList.add(styles.turn);
+  //         setTimeout(() => {
+  //           page.style.zIndex = (20 + 1).toString();
+  //         }, 500);
+  //       }
+  //     }, (index + 1) * 200 + 100);
+  //   })
+
+  // })
+
+  const handleTurnPage = (page: string, index: number) => {
+
+    const pageId = document.getElementById(page);
+
+    if (pageId?.classList.contains(styles.turn)) {
+      pageId?.classList.remove(styles.turn);
+      setTimeout(() => {
+        if (pageId) {
+          pageId.style.zIndex = (20 - index).toString();
+        }
+      }, 500);
+    } else {
+      pageId?.classList.add(styles.turn);
+      setTimeout(() => {
+        if (pageId) {
+          pageId.style.zIndex = (20 + index).toString();
+        }
+      }, 500);
+    }
+  }
+
+  const pageSection = [
+    { frontPage: <h1>Page1</h1>, backPage: <h1>Page2</h1> },
+    { frontPage: <h1>Page4</h1>, backPage: <h1>Page5</h1> },
+    { frontPage: <h1>Page6</h1> , backPage: <h1>Page7</h1> },
+  ]
+
+
+  useEffect(() => {
+    const pages = document.querySelectorAll(`.${styles["book-page"]}.${styles["page-right"]}`);
+    const totalPage = pages.length;
+    let currentPage = 0;
+
+    const reversePage = () => {
+      currentPage--;
+      if (currentPage < 0) {
+        currentPage = totalPage - 1;
+      }
+    };
+
+
+    const coverRight = document.querySelector<HTMLElement>(`.${styles["cover-right"]}`);
+
+    if (coverRight) {
+      setTimeout(() => {
+        coverRight.classList.add(styles["turn"]);
+      }, 2100);
+
+      setTimeout(() => {
+        coverRight.style.zIndex = "-1";
+      }, 2800);
+    }
+
+    console.log("total page: ", totalPage);
+
+    pages.forEach((_, index) => {
+      setTimeout(() => {
+        reversePage();
+        (pages[currentPage] as HTMLElement).classList.remove(styles["turn"]);
+
+        setTimeout(() => {
+          reversePage();
+          (pages[currentPage] as HTMLElement).style.zIndex = (10 + index).toString();
+        }, 500);
+      }, (index + 1) * 200 + 2100);
+    });
+  }, []);
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      {isBigScreen ? <div className={styles.wrapper}>
+        <div className={[styles.cover, styles["cover-left"]].join(" ")}></div>
+        <div className={[styles.cover, styles["cover-right"]].join(" ")}></div>
+
+        <div className={styles.book}>
+          <div className={[styles["book-page"], styles["page-left"]].join(" ")}>
+            <h1>Page 0</h1>
+          </div>
+
+
+          {pageSection.map((page, index) => {
+            const pageId = `turn-${index + 1}`;
+            return (
+              <div
+                key={index + 1}
+                id={pageId}
+                className={[styles["book-page"], styles["page-right"], styles["turn"]].join(" ")}>
+                <div className={styles["page-front"]}>
+                  {page.frontPage}
+                  <FontAwesomeIcon className='next page-btn' icon={faCaretRight} onClick={() => { handleTurnPage(pageId, index) }} />
+                </div>
+                <div className={styles["page-back"]}>
+                  {page.backPage}
+                  <FontAwesomeIcon className='prev page-btn' icon={faCaretRight} onClick={() => { handleTurnPage(pageId, index + 1) }} />
+                </div>
+              </div>
+            )
+          })}
+
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </div> :
+        <div className={styles.apologize}>
+          <h2>We apologize, but this website is currently unavailable on this device. It will be available soon. The currently available device sizes are 10 inches or above, suitable for viewing on laptops and desktops.</h2>
+        </div>}
     </main>
   );
 }
